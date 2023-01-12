5efac79ecee58aa774c534d612ddc9c55d5c33c8
marek <marek@DESKTOP-H9HQV9G>
2023-01-12 13:22:56 +0100

    Přidejte soubory projektu.

---
+++ WinBrake/MainWindow.xaml.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Windows.Threading;
+using System.IO.Ports;
+using OxyPlot;
+using OxyPlot.Series;
+using OxyPlot.Axes;
+using OxyPlot.Wpf;
+using MB;
+
+namespace WinBrake
+{
+    /// <summary>
+    /// Interakční logika pro MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public PlotModel MyModel { get; private set; }//OxyPlot function   
+        public OxyPlot.Series.LineSeries I, D, V, U;//OxyPlot function
+  
+        public static DispatcherTimer ScanTimer;
+
+        public static Double t;       
+        public static  Int16 current, distance, voltage;
+        public static float revolution,revolinv,distgraf,voltgraf;
+        public static int n;
+        public static string[] ports;
+        public static List<string> portsList;
+        public static UInt16 SpacePar,GreasePar;
+        public static bool COMMAND;
+
+        private void SetPar_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            UInt16.TryParse(Space.Text,out SpacePar);
+            Modbus.HOLDREGS[0].w = SpacePar;
+            UInt16.TryParse(Grease.Text, out GreasePar);
+            Modbus.HOLDREGS[1].w = GreasePar;
+            Modbus.ReqStruc.unitId = 0x1;
+            Modbus.ReqStruc.funCode = 0x10;
+            Modbus.ReqStruc.startAdr.w = Modbus.HOLDADR0;
+            Modbus.ReqStruc.quantity.w = 4;
+            SetPar.Background = Brushes.LightPink;
+            COMMAND = true;
+        }
+
+       
+        private void GetPar_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            Modbus.ReqStruc.unitId = 0x1;
+            Modbus.ReqStruc.funCode = 0x3;
+            Modbus.ReqStruc.startAdr.w = Modbus.HOLDADR0;
+            Modbus.ReqStruc.quantity.w = 4;
+            GetPar.Background = Brushes.LightPink;
+            COMMAND = true;
+        }
+
+
+        //     public static SerialPort serial;
+        Modbus mbus;
+        public DateTime StarTime,LasTime;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+            this.MyModel = new PlotModel
+            {//OxyPlot function
+                Title = "MOTOR", TitleColor = OxyColor.FromArgb(0xff, 0xff, 0, 0),
+            };
+            MyModel.Axes.Clear();
+            MyModel.Axes.Add(new LinearAxis { Position = AxisPosition.Bottom, Minimum = 0, Maximum = 60.0, AbsoluteMinimum = -10.0, AbsoluteMaximum = 61.00 , Title = "t[s]" }); ;
+            MyModel.Axes.Add(new LinearAxis { Position = AxisPosition.Left, Minimum = -100, Maximum = 100, AbsoluteMinimum = -200, AbsoluteMaximum = 200, Title = "[%]" });
+            MyModel.Axes[1].TextColor = OxyColor.FromRgb((byte)0xff, 0, 0);
+            MyModel.Axes[1].TicklineColor = OxyColor.FromRgb((byte)0xff, 0, 0);
+            MyModel.Axes[1].TitleColor = OxyColor.FromRgb((byte)0xff, 0, 0);
+            MyModel.Axes[1].AxislineColor = OxyColor.FromRgb((byte)0xff, 0, 0);
+            I = new OxyPlot.Series.LineSeries();//OxyPlot function...
+            I.StrokeThickness = 1.5;
+            I.Title = "I";
+            I.Color = OxyColor.FromRgb(0xff, 0, 0);
+            I.TextColor= OxyColor.FromArgb(0xff,0xff, 0, 0);
+            I.FontSize = 12;
+            I.Font = "normal";
+            this.MyModel.Series.Add(I);
+            D = new OxyPlot.Series.LineSeries();
+            D.Color = OxyColor.FromRgb(0, 0xff, 0);
+            D.StrokeThickness = 1.5;
+            this.MyModel.Series.Add(D);
+            V = new OxyPlot.Series.LineSeries();
+            V.Color = OxyColor.FromRgb(0,0, 0xff);
+            V.StrokeThickness = 1.5;
+            this.MyModel.Series.Add(V);        
+            U = new OxyPlot.Series.LineSeries();
+            U.Color = OxyColor.FromRgb(0xff, 0x7f, 0);
+            U.StrokeThickness = 1.5;
+            this.MyModel.Series.Add(U);
+            ScanTimer = new DispatcherTimer();
+            ScanTimer.Tick += new EventHandler(ScanTimer_Tick);
+            ScanTimer.Interval = new TimeSpan(0, 0, 0, 0, 250);//  250ms interval
+            ScanTimer.Start();//t
+            mbus = new Modbus();
+            COM.Content = MB.ser.serial.PortName;
+            Modbus.MBrec += new Modbus.rec(RewP);
+            Modbus.INPREGS[0].w = 0x22;//t
+            LasTime=StarTime = DateTime.Now;
+       
+
+        }
+        public void RewP()
+        {
+            if (B.CRCERR)
+            {
+                B.CRCERR = false;
+                COM.Background = Brushes.LightPink;
+            }
+            else if(COMMAND)
+            {
+                COMMAND = false;
+                if (Modbus.RespStruc.funCode == 0x3)
+                {
+                    GetPar.Background = Brushes.LightGreen;
+                    Space.Text = Modbus.HOLDREGS[0].w.ToString();
+                    Grease.Text = Modbus.HOLDREGS[1].w.ToString();
+                }
+                else
+                {
+                    SetPar.Background = Brushes.LightGreen;
+                }
+            }
+            else
+            {
+                COM.Background = Brushes.LightGreen;
+                current = (Int16)Modbus.INPREGS[0].w;
+                distance = (Int16)Modbus.INPREGS[1].w;
+                distgraf = (int)distance >> 6;
+                revolinv = Modbus.ConvRegsToFloat(Modbus.INPREGS, 2);
+                if (revolinv != 0)
+                    revolution = (((3800000 / 2) / 2) / revolinv);
+                voltage = (Int16)Modbus.INPREGS[4].w;
+                voltgraf = (int)voltage >> 4;
+                TimeSpan time = DateTime.Now - StarTime;
+                TimeSpan dtime = DateTime.Now - LasTime;
+                // t += 0.1;
+                t = time.TotalMilliseconds/1000;
+                if (dtime.TotalSeconds >= 60)
+                {
+                    MyModel.Axes[0].Minimum = time.TotalSeconds;
+                    MyModel.Axes[0].AbsoluteMinimum = time.TotalSeconds-1;
+                    MyModel.Axes[0].Maximum = time.TotalSeconds+60;
+                    MyModel.Axes[0].AbsoluteMaximum = time.TotalSeconds+61;
+                    LasTime = DateTime.Now;
+                  //  t = 0;
+                    I.Points.Clear();
+                    D.Points.Clear();
+                    V.Points.Clear();
+                    U.Points.Clear();
+                }
+                I.Points.Add(new DataPoint(t, current));
+                D.Points.Add(new DataPoint(t, distgraf));
+                V.Points.Add(new DataPoint(t, revolution));
+                U.Points.Add(new DataPoint(t, voltgraf));
+                this.MyModel.InvalidatePlot(true);
+            }
+
+        }
+
+        public void ScanTimer_Tick(object sender, EventArgs et)
+        {
+            try
+            {
+                if (COMMAND)
+                {
+                  //  Space.Background = Brushes.LightPink;
+                }
+                else
+                {
+                    Modbus.ReqStruc.unitId = 0x1;
+                    Modbus.ReqStruc.funCode = 0x4;
+                    Modbus.ReqStruc.startAdr.w = (ushort)(Modbus.INPADR0);
+                    Modbus.ReqStruc.quantity.w = 5;              
+                }
+                Modbus.Require(Modbus.ReqStruc);
+            }            
+            catch (Exception e)
+            {
+                System.Windows.MessageBox.Show("Timer tick except:\n" + e.ToString());
+
+            }
+        }
+    }
+}
---
+++ WinBrake/Modbus.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+using System.Threading;
+
+namespace MB
+{
+    public struct MB
+    {
+     //   public bool request;
+     //   public bool response;
+     //   public WORD length;
+        public byte unitId;
+        public byte funCode;
+        public WORD startAdr;
+        public WORD quantity;
+        public byte byteCount;
+      //  public byte exceptionCode;
+        public byte errorCode;
+      //  public WORD[] receiveCoilValues;
+      //  public WORD[] receiveRegisterValues;
+      //  public Int16[] sendRegisterValues;
+      //  public bool[] sendCoilValues;
+        public WORD crc;
+        public ushort words;
+    }
+    public class Modbus
+    {
+        static int i;
+        const int LH = 64;
+        const int LI = 64;
+        const int LM = 64;
+        public static WORD[] HOLDREGS = new WORD[LH];
+        public static WORD[] INPREGS = new WORD[LI];
+        public static ushort HOLDADR0 = 40000;
+        public static ushort INPADR0 = 30000;
+        public static MB ReqStruc, RespStruc,rs;
+        public delegate void rec();
+        public static event rec MBrec;
+        public Modbus()
+        {
+            ser Ser = new ser();
+            ser.Timer.Tick += new EventHandler(Timer_Tick);
+        }
+        public void Timer_Tick(object sender, EventArgs et)
+        {
+            try
+            {
+                B.j = B.i;
+                B.i = 0;
+                ser.Timer.Stop();
+                if (ser.BUSYC)
+                {
+                    ser.CRCread = ser.calculateCRC(B.bufin, (ushort)B.j, 0);
+                    if (ser.CRCread.w == 0)
+                    {
+                        B.CRCERR = false;
+                        B.NO = 0;       //counter of bad received frames
+                        rs=Response();//t                  
+                    }
+                    else
+                        B.CRCERR = true;
+                    B.j--;
+                    MBrec();//event after pause
+                    B.j = 0;
+                    ser.BUSYC = false;
+                }
+            }
+            catch (Exception e)
+            {
+                System.Windows.MessageBox.Show("Modbus timer tick except:\n" + e.ToString());
+
+            }
+
+        }
+
+
+        //copy  two 2-bytes word registers  into 4-byte single float format into 
+        public static float ConvRegsToFloat(WORD[] reg, int u)
+        {
+            byte[] floatBytes = {
+                                   reg[u].L,
+                                   reg[u].H,
+                                   reg[u+1].L,
+                                   reg[u+1].H
+                                };
+            return BitConverter.ToSingle(floatBytes, 0);
+        }
+
+        //copy  4-byte single float format into two 2-bytes word registers  
+        public static void ConvFloatToRegs( float f, WORD[] reg, int u)
+        {
+            byte[] b = BitConverter.GetBytes(f);
+            reg[u].L = b[0];
+            reg[u].H = b[1];
+            reg[u + 1].L= b[2];
+            reg[u + 1].H = b[3];
+        }
+
+        //aux. func: copy modbus registers into  comm. buffer
+        public static int WBcopy(WORD[] HREGS, ushort firstadr, byte[] buf, int firstbyte, ushort nums)//copy word into modbus protocol buffer
+        {
+            i = firstbyte;
+            ushort lastadr = firstadr;
+            lastadr += nums;
+            for (ushort adr = firstadr; adr < lastadr; adr++)
+            {
+                buf[i] = HREGS[adr].H;
+                i++;
+                buf[i] = HREGS[adr].L;
+                i++;
+            }
+
+            return i;
+        }
+
+        //aux. func:copy comm. buffer into modbus registers   
+        public static int BWcopy(byte[] buf, int firstbyte, WORD[] HREGS, ushort firstadr, ushort nums)
+        {
+            i = firstbyte;  
+            ushort lastadr = firstadr;
+            lastadr += nums;
+            for (ushort adr = firstadr; adr < lastadr; adr++)
+            {
+                if(i < B.LB && adr< LM)
+                 HREGS[adr].H = buf[i];
+                i++;
+                if (i < B.LB && adr < LM)
+                    HREGS[adr].L= buf[i];
+                i++;
+            }
+
+            return i;
+        }
+
+        //modbus client send frame to server 
+        // public static void Require(byte unitId, byte funCode, ushort startAdr, ushort quantity)
+        public static void Require(MB ReqStruc)
+        {
+           try
+           { 
+            int i = 0;
+          //  ReqStruc.unitId = unitId;
+            B.bufout[i] = ReqStruc.unitId;
+            i++;
+          //  ReqStruc.funCode = funCode;
+            B.bufout[i] = ReqStruc.funCode;
+            i++;
+          //  ReqStruc.startAdr.w = startAdr;
+          //  ReqStruc.quantity.w = quantity;
+            ReqStruc.byteCount = (byte)((ReqStruc.quantity.w & 0x7f) << 1);
+            switch (ReqStruc.funCode)
+            {
+                case (0x3):
+                    B.bufout[i] = ReqStruc.startAdr.H;
+                    i++;
+                    B.bufout[i] = ReqStruc.startAdr.L;
+                    i++;
+                    B.bufout[i] = ReqStruc.quantity.H;
+                    i++;
+                    B.bufout[i] = ReqStruc.quantity.L;
+                    i++;
+                    break;
+                case (0x4):
+                    B.bufout[i] = ReqStruc.startAdr.H;
+                    i++;
+                    B.bufout[i] = ReqStruc.startAdr.L;
+                    i++;
+                    B.bufout[i] = ReqStruc.quantity.H;
+                    i++;
+                    B.bufout[i] = ReqStruc.quantity.L;
+                    i++;
+                    break;
+                case (0x10):
+                    B.bufout[i] = ReqStruc.startAdr.H;
+                    i++;
+                    B.bufout[i] = ReqStruc.startAdr.L;
+                    i++;
+                    B.bufout[i] = ReqStruc.quantity.H;
+                    i++;
+                    B.bufout[i] = ReqStruc.quantity.L;
+                    i++;
+                    B.bufout[i] = ReqStruc.byteCount;
+                    i++;
+                    i = WBcopy(HOLDREGS, (ushort)(ReqStruc.startAdr.w- HOLDADR0), B.bufout, i, ReqStruc.quantity.w);
+                    break;
+            }
+            WORD CRC = ser.calculateCRC(B.bufout, (ushort)i, 0);
+            B.bufout[i] = CRC.L;
+            i++;
+            B.bufout[i] = CRC.H;
+            i++;
+            CRC = ser.calculateCRC(B.bufout, (ushort)i, 0);     //test
+            ser.Write(B.bufout, i);
+         }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show("Require except:\n"+ ex.ToString());
+
+            }
+
+        }
+
+        ///modbus client has accepted frame from server
+        public static MB Response()
+        {
+            try
+            {
+                int i = 0;
+                ushort u;
+                RespStruc.unitId = B.bufin[i];
+                i++;
+                RespStruc.errorCode = 0;
+                RespStruc.funCode = B.bufin[i];
+                i++;
+                switch (RespStruc.funCode)
+                {
+                    case (0x3):
+                        RespStruc.startAdr = ReqStruc.startAdr;
+                        RespStruc.byteCount = B.bufin[i];
+                        i++;
+                        u = RespStruc.byteCount;
+                        RespStruc.words = (ushort)(RespStruc.byteCount);
+                        RespStruc.words >>= 1;
+                        i = BWcopy(B.bufin, i, HOLDREGS, (ushort)(RespStruc.startAdr.w - HOLDADR0), RespStruc.words);
+                        break;
+                    case (0x4):
+                        RespStruc.startAdr = ReqStruc.startAdr;
+                        RespStruc.byteCount = B.bufin[i];
+                        i++;
+                        u = RespStruc.byteCount;
+                        RespStruc.words = (ushort)(RespStruc.byteCount);
+                        RespStruc.words >>= 1;
+                        i = BWcopy(B.bufin, i, INPREGS, (ushort)(RespStruc.startAdr.w - INPADR0), RespStruc.words);
+                        break;
+                    case (0x10):
+                        RespStruc.startAdr.H = B.bufin[i];
+                        i++;
+                        RespStruc.startAdr.L = B.bufin[i];
+                        i++;
+                        RespStruc.quantity.H = B.bufin[i];
+                        i++;
+                        RespStruc.quantity.L = B.bufin[i];
+                        i++;
+                        break;
+                    default:
+                        if((RespStruc.funCode & 0x80) !=0)
+                        {
+                            RespStruc.errorCode= B.bufin[i];
+                            i++;
+                        }
+
+                        break;
+                }
+                RespStruc.crc.H= B.bufin[i];
+                i++;
+                RespStruc.crc.L = B.bufin[i];
+                i++;
+
+            }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show("Modbus response except:\n"+ ex.ToString());
+
+            }
+            return RespStruc;
+
+        }
+
+
+    }
+
+}
